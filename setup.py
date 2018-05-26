5a17eb65a3d0b3f6f9aa0f12caa22ca67f0487fc
Jimmy Lu <gongchuo.lu@gmail.com>
2018-05-26 11:27:32 -0400

    Upload to PyPI

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(name='dfcompare',
+                 version='0.1.0',
+                 packages=['dfcompare'],
+                 url='https://github.com/Yuhta/dfcompare',
+                 author="Jimmy Lu",
+                 author_email="gongchuo.lu@gmail.com")
